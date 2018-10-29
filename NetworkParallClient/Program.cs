186d43bbca2b2b6721aacec5a7a9a99e0ad1ca37
medmene <38806186+medmene@users.noreply.github.com>
2018-10-29 13:10:42 +0700

    Add files via upload

--- NetworkParallClient/Program.cs
+++ NetworkParallClient/Program.cs
@@ -1,3 +1,4 @@
+﻿#define Multithread
 using System;
 using System.Text;
 using System.Net;
@@ -12,7 +13,55 @@ namespace SocketClient
 {
     class Program
     {
-        
+#if Multithread
+        const int port = 8888;
+        const string address = "127.0.0.1";
+        static void Main(string[] args)
+        {
+            //Console.Write("Введите свое имя:");
+            string userName = Console.ReadLine();
+            TcpClient client = null;
+            try
+            {
+                client = new TcpClient(address, port);
+                NetworkStream stream = client.GetStream();
+ 
+                while (true)
+                {
+                    Console.Write(userName + ": ");
+                    // ввод сообщения
+                    string message = Console.ReadLine();
+                    message = String.Format("{0}: {1}", userName, message);
+                    // преобразуем сообщение в массив байтов
+                    byte[] data = Encoding.Unicode.GetBytes(message);
+                    // отправка сообщения
+                    stream.Write(data, 0, data.Length);
+ 
+                    // получаем ответ
+                    data = new byte[64]; // буфер для получаемых данных
+                    StringBuilder builder = new StringBuilder();
+                    int bytes = 0;
+                    do
+                    {
+                        bytes = stream.Read(data, 0, data.Length);
+                        builder.Append(Encoding.Unicode.GetString(data, 0, bytes));
+                    }
+                    while (stream.DataAvailable);
+ 
+                    message = builder.ToString();
+                    Console.WriteLine("Сервер: {0}", message);
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            finally
+            {
+                client.Close();
+            }
+        }
+#else
         static void Main(string[] args)
         {
             try
@@ -160,7 +209,9 @@ namespace SocketClient
             //if (message.IndexOf("<TheEnd>") == -1)
             //SendMessageFromSocket(port);
 #endif
- 
-        }        
+
+        }
+
+#endif
     }
 }
